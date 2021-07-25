05eb4428f17f10308b970a281114e3a070a0e4af
geoff <geoff@LAPTOP-M64G9J56>
2021-07-25 13:12:56 +0530

    initial-commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,28 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+
+        [Fact]
+        public void TEST_GET()
+        {
+
+
+          //Arrange
+            var newSum=2;
+
+            //Act
+             var neDat=1+1;
+
+            //Assert
+
+            Assert.Equal(neDat,newSum);
+
+        }
+    }
+}
